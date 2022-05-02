7b3e65baea7f6536c7efbe350775eb4a63b1dd52
Josip Keresman <josipkeresman@gmail.com>
2022-05-02 19:19:03 +0200

    Commit

--- functions/__init__.py
+++ functions/__init__.py
@@ -2,7 +2,7 @@ from exceptions import NotPositiveNumberError
 from player import Player, FootballPlayer, BasketballPlayer
 
 
-def new_player():
+def new_player() -> Player:
     first_name = input("Enter player's first name: ")
     last_name = input("Enter player's last name: ")
     height = float(input("Enter player's height: "))
@@ -11,7 +11,7 @@ def new_player():
     return Player(first_name=first_name, last_name=last_name, height_cm=height, weight_kg=weight)
 
 
-def new_football_player():
+def new_football_player() -> FootballPlayer:
     while True:
         try:
             player = new_player()
@@ -28,7 +28,7 @@ def new_football_player():
     return FootballPlayer(player=player, goals=goals, yellow_cards=yellow_cards, red_cards=red_cards)
 
 
-def new_basketball_player():
+def new_basketball_player() -> BasketballPlayer:
     while True:
         try:
             player = new_player()
@@ -43,4 +43,3 @@ def new_basketball_player():
     assists = int(input("Enter number of assists: "))
 
     return BasketballPlayer(player=player, points=points, rebounds=rebounds, assists=assists)
-
